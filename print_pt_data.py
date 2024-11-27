8306b3025cf00e6771cab4c1446ad2fd43800c2e
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-11-27 11:21:00 +0100

    Add files via upload

---
+++ print_pt_data.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+''' Structured reading and writing data from .pt file.'''
+#
+# Version 0.0.0.1
+#
+# pylint: disable=wrong-import-position
+# pylint: disable=consider-using-sys-exit
+
+# Import module.
+import warnings
+
+# Ignore (torch) future warning.
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# Import module.
+import torch
+
+# Set the file.
+PT_FILE = "RRDB_8xPSNR.pth"
+
+# Main script function.
+def main(filename):
+    '''Main script function'''
+    # Load pt file into data.
+    data = torch.load(filename)
+    # Loop over key and value in dict.
+    for param in data:
+        # print key and value.
+        print(param, data[param])
+
+# Execute as module as well as a program.
+if __name__ == "__main__":
+    # Call main function.
+    main(PT_FILE)
