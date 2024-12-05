16855a736e44dfee22c717643ef13b551b40a54e
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-12-05 14:55:00 +0100

    Add files via upload

--- tool_scripts/old_esrgan_analysis.py
+++ tool_scripts/old_esrgan_analysis.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
 '''Analysis of the old ESRGAN data structure.'''
+# pylint: disable=protected-access
 # pylint: disable=useless-return
 # pylint: disable=bare-except
 # pylint: disable=invalid-name
-# pylint: disable=protected-access
-# pylint: disable=too-many-nested-blocks
+# pylint: disable=consider-using-f-string
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-branches
+# pylint: disable=redefined-outer-name
 #
 # old ESRGAN Model Data Analysis
 # Version 0.0.0.1
 #
+# Description:
+# Th script is working on base of the ESRGAN models
+# I have worked with and I have seen so far.
+#
 # To-Do:
-# Sanitize and optimise script. Add a description.
-# 
+# Sanitize and optimise script.
 
 # Import standard Python modules.
 import re
 import os
 import sys
 import warnings
+from collections import OrderedDict
 
 # Import third party module.
 import torch
@@ -25,11 +32,7 @@ import torch
 # Ignore (torch) future warning.
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-# Set torch print options.
-torch.set_printoptions(threshold=10_000)
-
 # Get the model name from the command line.
-# To-Do: check if it is a file (zip/binary).
 # To-Do: check if it is a pt or pth file.
 if len(sys.argv) > 1:
     model_name = sys.argv[1]
@@ -38,7 +41,7 @@ else:
     print(exit_message)
     os._exit(1)
 
-# Set the color strings.
+# Define the color escape sequence strings.
 COL_DEFAULT = "\33[49m"
 COL_RED = "\33[41m"
 COL_GREEN = "\33[42m"
@@ -50,17 +53,17 @@ COL_GRAY = "\33[47m"
 COL_DARK_GRAY = "\33[100m"
 
 # Set some strings.
-error_str = "\n\33[41mERROR: Check the data structure!\33[49m"
-warn_str_0 = "\n\33[46mFound UNKNOWN (OLD) ESRGAN RRBD model. Check the data!\33[49m"
-warn_str_1 = "\n\33[46mFound UNKNOWN (NEW) ESRGAN RRBD model. Check the data!\33[49m"
-info_str_0 = "\n\33[45mFound old ESRGAN RRBD model\33[49m"
-info_str_1 = "\n\33[45mFound new ESRGAN RRBD model\33[49m"
+ERR_STR = "\n\33[41mERROR: Check the data structure!\33[49m"
+WARN_STR_0 = "\n\33[46mFound UNKNOWN (OLD) ESRGAN RRBD model. Check the data!\33[49m"
+WARN_STR_1 = "\n\33[46mFound UNKNOWN (NEW) ESRGAN RRBD model. Check the data!\33[49m"
+INFO_STR_0 = "\n\33[45mFound old ESRGAN RRBD model\33[49m"
+INFO_STR_1 = "\n\33[45mFound new ESRGAN RRBD model\33[49m"
 
 # Set some strings.
-weight_str_0 = 'model.0.weight'
-bias_str_0 = 'model.0.bias'
-weight_str_1 = 'conv_first.weight'
-bias_str_1 = 'conv_first.bias'
+WEIGHT_STR_0 = 'model.0.weight'
+BIAS_STR_0 = 'model.0.bias'
+WEIGHT_STR_1 = 'conv_first.weight'
+BIAS_STR_1 = 'conv_first.bias'
 
 # Define the regular expressions.
 body_regex = r"^model.1.sub.\b([0-9]|[1][3-9]|[12][0-2])\b.RDB[1-3].conv[0-5].0.(?:weight|bias)$"
@@ -70,25 +73,19 @@ head_foot_regex = r"^model.(1.sub.23|[01368]+[0]*).(?:weight|bias)$"
 pattern_body = re.compile(body_regex)
 pattern_head_foot = re.compile(head_foot_regex)
 
-# Set key word dict.
-key_word_dict = {"RRDB_trunk.0.": "NEW ESRGAN",
-                 "model.1.sub.": "OLD ESRGAN",
-                 "body.": "RealESRGAN"}
-key_word_list = list(key_word_dict.keys())
+# Set keyword dict and keyword list.
+keyword_dict = {"RRDB_trunk.0.": "NEW ESRGAN",
+                "model.1.sub.": "OLD ESRGAN",
+                "body.": "RealESRGAN"}
+keyword_list = list(keyword_dict.keys())
 
 # Set pre/post tensor dict and pre/post string list.
-ppdict = {"model.0.weight": [64, 3, 3, 3],
-          "model.0.bias": [64],
-          "model.1.sub.23.weight": [64, 64, 3, 3],
-          "model.1.sub.23.bias": [64],
-          "model.3.weight": [64, 64, 3, 3],
-          "model.3.bias": [64],
-          "model.6.weight": [64, 64, 3, 3],
-          "model.6.bias": [64],
-          "model.8.weight": [64, 64, 3, 3],
-          "model.8.bias": [64],
-          "model.10.weight": [3, 64, 3, 3],
-          "model.10.bias": [3]}
+ppdict = {"model.0.weight": [64, 3, 3, 3], "model.0.bias": [64],
+          "model.1.sub.23.weight": [64, 64, 3, 3], "model.1.sub.23.bias": [64],
+          "model.3.weight": [64, 64, 3, 3], "model.3.bias": [64],
+          "model.6.weight": [64, 64, 3, 3], "model.6.bias": [64],
+          "model.8.weight": [64, 64, 3, 3], "model.8.bias": [64],
+          "model.10.weight": [3, 64, 3, 3], "model.10.bias": [3]}
 pplist = list(ppdict.keys())
 
 # New approach.
@@ -132,6 +129,7 @@ def clear_term() -> None:
 # PK = \x50\x4B
 # --------------------
 def file_type(filename):
+    '''Get file type.'''
     file_type = "unknown"
     with open(filename, 'rb') as file:
         content = file.read(2)
@@ -185,7 +183,7 @@ def check_tensors(data):
                     list(data[key].size()) == v[PAT_W]:
                     ret_arr.remove(key)
                     break
-                elif k in key and PAT_B in key and \
+                if k in key and PAT_B in key and \
                     list(data[key].size()) == v[PAT_B]:
                     ret_arr.remove(key)
                     break
@@ -212,7 +210,7 @@ def check_pre_pos(data):
                     if key == k and list(data[key].size()) == v:
                         ret_arr.remove(key)
                         break
-            elif PAT_B in key:
+            if PAT_B in key:
                 for k, v in ppdict.items():
                     if key == k and list(data[key].size()) == v:
                         ret_arr.remove(key)
@@ -227,27 +225,41 @@ def check_pre_pos(data):
 # Function simple_check()
 # -----------------------
 def simple_check(model_content):
+    '''Simple check.'''
     # Perform some sipmple checks.
+    isOLDW = None
+    isOLDB = None
+    isNEWW = None
+    isNEWB = None
     weight_shape_ref = [64, 3, 3, 3]
     bias_shape_ref = [64]
-    if weight_str_0 in model_content and bias_str_0 in model_content:
-        weight_shape = list(model_content[weight_str_0].size())
-        bias_shape = list(model_content[bias_str_0].size())
-        if weight_shape == weight_shape_ref and bias_shape == bias_shape_ref:
-            print(info_str_0)
-        else:
-            print(warn_str_0)
-    elif weight_str_1 in model_content and bias_str_1 in model_content:
-        weight_shape = list(model_content[weight_str_1].size())
-        bias_shape = list(model_content[bias_str_1].size())
-        if weight_shape == weight_shape_ref and bias_shape_ref == [64]:
-            print(info_str_1)
-        else:
-            print(warn_str_1)
+    for key in model_content:
+        # Check on old.
+        if WEIGHT_STR_0 == key:
+            weight_shape = list(model_content[WEIGHT_STR_0].size())
+            isOLDW = bool(weight_shape == weight_shape_ref)
+        if BIAS_STR_0 == key:
+            bias_shape = list(model_content[BIAS_STR_0].size())
+            isOLDB = bool(bias_shape == bias_shape_ref)
+        # Check on new.
+        if WEIGHT_STR_1 == key:
+            weight_shape = list(model_content[WEIGHT_STR_1].size())
+            isNEWW = bool(weight_shape == weight_shape_ref)
+        if BIAS_STR_1 == key:
+            bias_shape = list(model_content[BIAS_STR_1].size())
+            isNEWB = bool(bias_shape == bias_shape_ref)
+    # Check the possible cases.
+    if isOLDW and isOLDB is not None and bool(isOLDW == isOLDB):
+        print(INFO_STR_0)
+    elif isNEWW and isNEWB is not None and bool(isNEWW == isNEWB):
+        print(INFO_STR_1)
+    elif isOLDW or isOLDB is not None and bool(isOLDW == isOLDB):
+        print(WARN_STR_0)
+    elif isNEWW or isNEWB is not None and bool(isOLDW == isOLDB):
+        print(WARN_STR_1)
     else:
-        # Print the content to the terminal window.
         print(model_content)
-        print(error_str)
+        print(ERR_STR)
     # Return None
     return None
 
@@ -257,7 +269,8 @@ def simple_check(model_content):
 def check_esrgan(model_data):
     '''Main script function'''
     # Perform some checks.
-    print("\n***  key and tensor checks  ***")
+    msg_str = "{0}{1}".format("\n", "***  key and tensor checks  ***")
+    print(msg_str)
     chkval0, chkarr0, cnt0 = check_keys(model_data)
     chkval1, chkarr1, cnt1 = check_tensors(model_data)
     chkval2, chkarr2, cnt2 = check_pre_pos(model_data)
@@ -265,15 +278,15 @@ def check_esrgan(model_data):
     if chkarr0:
         print("\n{}".format(chkarr0))
     else:
-        print("Key check ok. Noting to print out!")
+        print("\nKey check ok. Nothing to print out!")
     if chkarr1:
         print("\n{}".format(chkarr1))
     else:
-        print("Tensor check ok. Noting to print out!")
+        print("\nTensor check ok. Nothing to print out!")
     if chkarr2:
         print("\n{}".format(chkarr2))
     else:
-        print("Pre/Post key check ok. Noting to print out!")
+        print("\nPre/Post key check ok. Nothing to print out!")
     # Check on mismatching lengths.
     if len(chkarr0) != cnt0 and len(chkarr0) > 0:
         print("\nMismatch in number of keys!")
@@ -301,13 +314,45 @@ def check_esrgan(model_data):
     # Return None
     return None
 
+# ---------------------
+# Function print_keys()
+# ---------------------
+def print_keys(model_content):
+    '''Print keys from the model dict.'''
+    # Print message into the terminal window.
+    msg_str = "{0}{1}{2}".format("\n", "***  keys and tensor shapes  ***", "\n")
+    print(msg_str)
+    # Loop over they keys and print keyes ans tensor shape.
+    for key in model_content:
+        dim = list(model_content[key].size())
+        print("%-32s%s" % (key, dim))
+    # Return None.
+    return None
+
+# -------------------------
+# Function get_model_type()
+# -------------------------
+def get_model_type(model_content):
+    '''Get model type.'''
+    for key in model_content:
+        for k in keyword_list:
+            if k in key:
+                print("\n***  model type prediction  ***\n")
+                print("Possible model type:", keyword_dict[k])
+                break
+        else:
+            continue
+        break
+    return None
+
 # ++++++++++++++++++++
 # Main script function
 # ++++++++++++++++++++
 def main(filename):
     '''Main script function'''
-    # print file type.
-    print("***  file type  ***\n")
+    # Print file type for analysis purposes.
+    msg_str = "{0}{1}".format("***  file type  ***", "\n")
+    print(msg_str)
     print("File Type:", file_type(filename))
     # Try to load the model data from the file.
     # Must be of type collections.OrderedDict.
@@ -319,11 +364,16 @@ def main(filename):
         print(err_msg)
         return None
     # Check if it is dict or OrderedDict.
-    if str(type(model_content)) == "<class 'collections.OrderedDict'>":
-        pass
-    elif str(type(model_content)) == "<class 'dict'>":
+    if isinstance(model_content, OrderedDict):
+        get_model_type(model_content)
+    elif isinstance(model_content, dict):
+        get_model_type(model_content)
         key_var = list(model_content.keys())[0]
-        if key_var == "params_ema" or key_var == "params":
+        for keyword in keyword_list:
+            if keyword in model_content[key_var]:
+                print("Possible model type:", keyword_dict[keyword])
+        #if key_var == "params_ema" or key_var == "params":
+        if key_var in ("params_ema", "params"):
             warn_str = "Take a look at the data structure. Maybe it is a RealESRGAN model!\33"
             warn_msg = "{0}{1}{2}".format(COL_CYAN, warn_str, COL_DEFAULT)
             print(warn_msg)
@@ -331,13 +381,10 @@ def main(filename):
             warn_str = "Unknown model type. Take a look at the data structure!\33"
             warn_msg = "{0}{1}{2}".format(COL_CYAN, warn_str, COL_DEFAULT)
             print(warn_msg)
-        # return None
+        # Return None
         return None
-    # Loop over they keys and print keyes ans tensor shape.
-    print("\n***  keys and tensor shapes  ***\n")
-    for key in model_content:
-        dim = list(model_content[key].size())
-        print(key, dim)
+    # Print model keys.
+    print_keys(model_content)
     # Call check function.
     check_esrgan(model_content)
     # Return None
@@ -345,7 +392,7 @@ def main(filename):
 
 # Execute as module as well as a program.
 if __name__ == "__main__":
-    # Reset terminal.
+    # Reset the terminal window.
     reset_term()
     # Call the main function.
     main(model_name)
